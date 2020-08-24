12201baca009130ea9544fea7ad623086b31f2d9
Carlos Placencia <carlosplacencia2012@gmail.com>
2020-08-24 17:17:45 +0000

    new route

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send("Hello World");
+	res.send("Hello my name is Carlos");
+});
+
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', '4life');
+	res.status(418);
+	res.send("I'm Working!");
 });
 
 app.listen(port, () => console.log(`fullStack listening on port ${port}!`));
